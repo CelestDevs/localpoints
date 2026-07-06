bc18cd9a3b6a8a58e51a30d63b049bdcfcd56ca0
Celeste Developers <celestdevs@gmail.com>
2026-07-06 10:42:46 -0300

    V9

--- public/sw.js
+++ public/sw.js
@@ -7,7 +7,7 @@
 // ═══════════════════════════════════════════════════════════════
 
 // ATENÇÃO: mude este valor a cada deploy para invalidar o cache
-const BUILD_ID    = '060720261034';
+const BUILD_ID    = '060720261042';
 const CACHE_NAME  = 'localpoints-v' + BUILD_ID;
 
 // Recursos estáticos que podem ser cacheados com segurança
